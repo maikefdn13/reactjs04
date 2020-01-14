2bf233fe276dd3fdfcf9d5f62bfbff97fb277a30
Jose Maike Rodrigues de Sousa <maikerodrigues@cialne.com.br>
2020-01-14 09:25:49 -0300

    Finalizado

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -1,14 +1,34 @@
 import React, { Component } from 'react';
 
+import TechItem from './TechItem';
+
 class TechList extends Component {
   state = {
     newTech: '',
-    techs: [
-     'Node.js',
-      'ReactJS',
-      'React Native'
-    ]
+    techs: []
   };
+
+  // Executado assim que o componente aparece em tela
+  componentDidMount() {
+    const techs = localStorage.getItem('techs');
+
+    if(techs) {
+      this.setState({ techs: JSON.parse(techs) });
+    }
+  }
+
+  // Executado sempre que houver alterações nas props ou estado
+  componentDidUpdate(_, prevState) {
+    //this.props, this.state
+    if(prevState.techs !== this.state.techs) {
+      localStorage.setItem('techs', JSON.stringify(this.state.techs))
+    }
+  }
+
+  // Executado quando o componente deixa de existir 
+  componentWillUnmount() {
+
+  }
 
   handleInputChange = e => {
     this.setState({ newTech: e.target.value }) 
@@ -33,11 +53,12 @@ class TechList extends Component {
       <h1>{this.state.newTech}</h1>
       <ul>
         {this.state.techs.map(tech => ( 
-          <li key={tech}>
-            {tech}
-            <button onClick={() => this.handleDelete(tech)} type="button">Remover</button>
-          </li> 
-        ))}
+          <TechItem 
+            key={tech} 
+            tech={tech} 
+            onDelete={() => this.handleDelete(tech) } 
+          />          
+        ))}    
       </ul>
       <input 
         type="text" 
